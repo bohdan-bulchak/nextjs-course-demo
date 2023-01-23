44611cef3dbde5531053236f218681d565b5eb6c
Bohdan Bulchak <bohdan.bulchak@student.tuke.sk>
2023-01-23 20:45:34 +0100

    ready for deployment

---
+++ src/pages/api/new-meetup.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://bohdanbulchak:a0973221261@cluster0.bgqqfbx.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup insertred!" });
+  }
+}
+
+export default handler;
